bfb3cf33fc421c98e5b9cc97f00e3e2f49841c34
GermanRuelas <lgruelas@hotmail.com>
2016-09-29 02:00:07 -0500

    Licence

--- main.cpp
+++ main.cpp
@@ -1,10 +1,24 @@
 /*
  * Genetic Algorithm 
  * Differential Evolution 
- * Germán Ruelas Luna
  * lgruelas@hotmail.com
  * 
  * to compile please une c++11 flags
+ * /*
+ * Copyright 2016 Germán Ruelas
+ * Author: Germán Ruelas Luna
+ * Licence: GPL version 3
+ * 
+ * This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
  */
 
 #include <iostream>

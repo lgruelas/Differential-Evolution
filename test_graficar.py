8f338b778032a062031499d799f2999e85c6a292
Germán Ruelas <lgruelas@hotmail.com>
2018-08-17 13:02:29 -0500

    adding more test cases

--- test_graficar.py
+++ test_graficar.py
@@ -2,3 +2,7 @@ import graficar
 
 def test_c_output():
     assert(type(graficar.run_cpp())==list)
+
+def test_individual_c_output():
+    a = graficar.run_cpp()
+    assert(type(a[0])==type(a[1])==type(a[2])==type(a[3])==float)

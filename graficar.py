35fa59b9db29a066a8c65d18724041ca86c20281
Germán Ruelas <lgruelas@hotmail.com>
2018-08-19 14:54:08 -0500

    Added license fixes

--- graficar.py
+++ graficar.py
@@ -1,6 +1,27 @@
 #! /usr/bin/python
 # -*- coding: utf-8 -*-
 
+#Copyright (C) 2018  Luis Germán Ruelas Luna gruelas@cieco.unam.mx
+
+#This file is part or Differential-Evolution
+
+#Differential-Evolution is free software; you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation; either version 3 of the License, or
+#(at your option) any later version.
+
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+
+#You should have received a copy of the GNU General Public License
+#along with this program; if not, write to the Free Software
+#Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+
+
 #really good parameters are [0.666681,0.499999,8.44622e-07]
 
 import auxiliar
